34a421a8361d3e9a9fd6459de969c296de8bb89a
ananyaa241 <brightside7806@gmail.com>
2026-05-20 08:02:10 +0530

    Update RootLayout.jsx

--- ATP-1/week-6/Sample-Project/frontend/src/components/RootLayout.jsx
+++ ATP-1/week-6/Sample-Project/frontend/src/components/RootLayout.jsx
@@ -1,15 +1,22 @@
-import Header from './Header'
-import {Outlet} from 'react-router-dom'
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
 
 function RootLayout() {
   return (
-    <div>
+    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-indigo-600">
+
+      {/* Header */}
       <Header />
-      <div className='min-h-screen mx-20 p-20 bg-blue-400'>
-        <Outlet />
-      </div>
+
+      {/* Main Content */}
+      <main className="px-4 sm:px-8 md:px-16 lg:px-20 py-8">
+        <div className="min-h-[85vh] rounded-3xl bg-white/10 backdrop-blur-sm shadow-xl p-6 sm:p-10">
+          <Outlet />
+        </div>
+      </main>
+
     </div>
-  )
+  );
 }
 
-export default RootLayout
+export default RootLayout;
